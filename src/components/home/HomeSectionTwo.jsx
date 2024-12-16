e2d36db9e183ccadb147c8aa53e6cadb7358b9d6
teekingtv2 <devteeking@gmail.com>
2024-12-16 12:16:56 +0100

    chum10

--- src/components/home/HomeSectionTwo.jsx
+++ src/components/home/HomeSectionTwo.jsx
@@ -13,7 +13,7 @@ const HomeSectionTwo = () => {
           alt=""
           className="w-full max-h-[800px]"
         />
-        <div className="absolute bg-[#3BB6B3] text-white py-3 lg:py-10 px-6 lg:px-16 flex justify-center items-center top-[38%] md:top-[45%] left-6 lg:left-[28%]">
+        <div className="absolute bg-[#3BB6B3] text-white py-3 lg:py-10 px-6 lg:px-16 flex justify-center items-center top-[40%] md:top-[45%] left-6 lg:left-[28%]">
           <TrackVisibility>
             {({ isVisible }) => (
               <div
