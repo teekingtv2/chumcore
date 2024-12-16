da5c72cb5de9bb9c00db032a2eb8ad59f030801d
teekingtv2 <devteeking@gmail.com>
2024-12-16 12:14:17 +0100

    chum8

--- src/components/home/HomeSectionTwo.jsx
+++ src/components/home/HomeSectionTwo.jsx
@@ -13,7 +13,7 @@ const HomeSectionTwo = () => {
           alt=""
           className="w-full max-h-[800px]"
         />
-        <div className="absolute bg-[#3BB6B3] text-white py-10 px-16 flex justify-center items-center top-[45%] left-[28%]">
+        <div className="absolute bg-[#3BB6B3] text-white py-6 lg:py-10 px-6 lg:px-16 flex justify-center items-center top-[38%] md:top-[45%] left-6 lg:left-[28%]">
           <TrackVisibility>
             {({ isVisible }) => (
               <div
@@ -23,7 +23,7 @@ const HomeSectionTwo = () => {
                     : ""
                 }
               >
-                <h1 className="text-[40px]">
+                <h1 className="text-[16px] lg:text-[40px]">
                   John says this cafe has the best latte!
                 </h1>
               </div>
